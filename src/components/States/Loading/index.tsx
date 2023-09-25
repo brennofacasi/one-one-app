0731b559f0cdfd92c581abf9cd9d0e2dc75e9b92
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-25 10:33:57 -0300

    feat: new loader and error states

---
+++ src/components/States/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { DotWave } from "@uiball/loaders";
+import styles from "./styles.module.scss";
+
+export default function Loading() {
+  return (
+    <div className={styles.wrapper}>
+      <DotWave size={32} color='#181a1b' />
+    </div>
+  );
+}
---
+++ src/components/States/index.tsx
@@ -0,0 +1,7 @@
+import Loading from "./Loading";
+import NotOk from "./NotOk";
+
+export const States = {
+  Loading: Loading,
+  NotOk: NotOk,
+};
