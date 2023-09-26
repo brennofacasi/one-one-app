aaa8c114cedf17f8902058fdc71cf03ef518a437
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-26 19:37:36 -0300

    fix: renamed function:

--- src/app/(app)/mentorias/layout.tsx
+++ src/app/(app)/mentorias/layout.tsx
@@ -1,6 +1,10 @@
 import { Header } from "@/components/Header";
 
-export default function Games({ children }: { children: React.ReactNode }) {
+export default function MeetingsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <Header.Topbar />
