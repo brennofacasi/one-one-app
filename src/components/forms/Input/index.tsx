6745be5fbd3035190c17474407826ce7eba58ac0
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-26 19:38:48 -0300

    fix: minor fixes

--- src/components/forms/Input/index.tsx
+++ src/components/forms/Input/index.tsx
@@ -10,7 +10,7 @@ export default function Input({
 }: InputProps) {
   return (
     <>
-      <label className={styles.label}>{label}</label>
+      {label && <label className={styles.label}>{label}</label>}
       <input
         className={styles.input}
         {...register(field, { required })}
