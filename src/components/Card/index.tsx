ecd7e3405c1b17cddb95b42f90344d666d567a8b
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-26 20:54:11 -0300

    feat: added custom class

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,13 @@
 import styles from "./styles.module.scss";
+import classNames from "classnames";
 
 interface CardProps {
   children: React.ReactNode;
+  newClass?: string;
 }
 
-export const Card = ({ children }: CardProps) => {
-  return <div className={styles.card__body}>{children}</div>;
+export const Card = ({ children, newClass }: CardProps) => {
+  return (
+    <div className={classNames(styles.card__body, newClass)}>{children}</div>
+  );
 };
