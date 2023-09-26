0337f67a79e8052f03b0abf89dd9648bb1335174
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-26 11:34:43 -0300

    feat: calendar first commit

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+
+function Calendar({
+  className,
+  classNames,
+  showOutsideDays = true,
+  ...props
+}: CalendarProps) {
+  return (
+    <DayPicker
+      showOutsideDays={showOutsideDays}
+      components={{
+        IconLeft: ({ ...props }) => <ChevronLeft className='h-4 w-4' />,
+        IconRight: ({ ...props }) => <ChevronRight className='h-4 w-4' />,
+      }}
+      {...props}
+    />
+  );
+}
+Calendar.displayName = "Calendar";
+
+export { Calendar };
