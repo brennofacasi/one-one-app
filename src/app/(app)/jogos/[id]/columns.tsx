1c52c6e0b1342d13e6643488ee992dac5c695c0c
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-26 19:40:53 -0300

    del: deleted game card

--- src/app/(app)/jogos/[id]/columns.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./styles.module.scss";
-
-export default function Columns({ children }: { children: React.ReactNode }) {
-  return <section className={styles.columns}>{children}</section>;
-}
