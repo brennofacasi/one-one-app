f00b3464c08470387d87d555a0d8e9dce154d7a2
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-26 11:30:13 -0300

    feat: added toastify and title template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 import Provider from "@/components/Provider";
-import "./globals.scss";
 import { Manrope } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import { Metadata } from "next";
+
+import "./globals.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Manrope({
   subsets: ["latin"],
   weight: ["300", "400", "700", "800"],
 });
 
-export const metadata = {
-  title: "Playbook Nouhau",
-  description: "Playbook Nouhau",
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Mentor App",
+    default: "Mentor App",
+  },
 };
 
 export default function RootLayout({
@@ -21,6 +27,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Provider>{children}</Provider>
+        <ToastContainer position='bottom-right' />
       </body>
     </html>
   );
