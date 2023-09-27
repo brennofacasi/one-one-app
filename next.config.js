bed9fc0191a094b9ba1e0718b1b99af9cef009d4
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-27 13:21:56 -0300

    feat: dockerized application

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: process.env.API_PORT,
-      },
-    ],
-  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
