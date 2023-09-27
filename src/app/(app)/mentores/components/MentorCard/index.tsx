054f102c22eb25e01f83eb9378a28cfbd1ba49a2
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-27 10:25:26 -0300

    feat: added create or edit modal

--- src/app/(app)/mentores/components/MentorCard/index.tsx
+++ src/app/(app)/mentores/components/MentorCard/index.tsx
@@ -1,20 +1,34 @@
 import { formatDate } from "@/utils/formatDate";
 import styles from "./styles.module.scss";
 import { MentorCardProps } from "./types";
-import { Button } from "@/components/forms/Button";
+import edit from "@/icons/edit.svg";
 import Modal from "@/components/Modal";
 import AvailabilitiesModal from "../AvailabilitiesModal";
+import CreateOrEditMentor from "../CreateOrEditMentor";
 
-export default function MentorCard({ data }: { data: MentorCardProps }) {
+export default function MentorCard({
+  data,
+  mutate,
+}: {
+  data: MentorCardProps;
+  mutate: () => void;
+}) {
   return (
     <div className={styles.body}>
       <div className={styles.wrapper}>
+        <div className={styles.edit__button}>
+          <Modal icon={edit} small>
+            <CreateOrEditMentor mutate={mutate} initialData={data} />
+          </Modal>
+        </div>
+
         <h5>
           {data.first_name} {data.last_name}
         </h5>
         <p>{data.email}</p>
         <small>Adicionado em {formatDate(data.created_at, "dd/MM/yyyy")}</small>
       </div>
+
       <Modal buttonContent='Ver disponibilidade' primary>
         <AvailabilitiesModal name={data.first_name} id={data.id} />
       </Modal>
